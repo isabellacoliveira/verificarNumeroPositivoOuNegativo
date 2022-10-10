a19b62e1f9909c9b5baa2ba1f590748850d5bbfd
Isabella Cruz de Oliveira <112203324+isabellacoliveira@users.noreply.github.com>
2022-10-10 14:41:59 -0300

    Add files via upload

---
+++ LpExercicio1.java
@@ -0,0 +1,24 @@
+package linguagemDeProgamacao;
+
+import java.util.Scanner;
+
+public class LpExercicio1 {
+		public static void main(String [] args) {
+			
+			Scanner uwu = new Scanner(System.in); 
+			
+			double numero;
+			System.out.println("digite um número"); 
+			numero = uwu.nextDouble(); 
+			
+			if (numero != 0) {
+				if (numero > 0 ) {
+					System.out.println("este número é positivo");
+				} else {
+					System.out.println("este número é negativo");
+		    } 
+	      }
+	  }
+	}
+
+
